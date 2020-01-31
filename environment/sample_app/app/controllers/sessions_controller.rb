7f5fcbf83cd4fca8fdc31c649a00239798349fcd
zonoman <morizono.2st@gmail.com>
2020-01-31 23:40:44 +0900

    9.2

--- environment/sample_app/app/controllers/sessions_controller.rb
+++ environment/sample_app/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email:params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user#sessionshelperのヘルパーメソッド。idをcokkieに保存する
-      remember user#sessionshelperのヘルパーメソッド。記憶トーキンのdigestをDBに保存、
+      params[:session][:remember_me] == '1'?remember(user):forget(user)
       redirect_to user#→user_url(user)
     else
       #エラーメッセージを作成する
