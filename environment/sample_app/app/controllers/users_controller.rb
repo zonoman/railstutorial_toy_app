8edd752686710f4667fd592bee7a45a41028a3a3
zonoman <morizono.2st@gmail.com>
2020-02-02 19:23:00 +0900

    10.5

--- environment/sample_app/app/controllers/users_controller.rb
+++ environment/sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-before_action :logged_in_user,only: [:edit,:update,:index]
+before_action :logged_in_user,only: [:edit,:update,:index,:destroy]
 before_action :correct_user, only: [:edit,:update]
+before_action :admin_user, only:[:destroy] 
+
   def new
     @user = User.new
   end
@@ -38,6 +40,12 @@ before_action :correct_user, only: [:edit,:update]
     @users = User.paginate(page:params[:page])
   end
   
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
 
 
   private 
@@ -64,8 +72,8 @@ before_action :correct_user, only: [:edit,:update]
       redirect_to(root_url) unless current_user?(@user)
     end
 
-
-
-
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 end
