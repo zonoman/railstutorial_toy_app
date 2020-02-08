1a0808f5d4f64f12d2425c7796fad5fb90592b28
zonoman <morizono.2st@gmail.com>
2020-02-08 16:11:37 +0900

    12.1

--- environment/sample_app/app/models/user.rb
+++ environment/sample_app/app/models/user.rb
@@ -64,6 +64,17 @@ class User < ApplicationRecord
         UserMailer.account_activation(self).deliver_now
     end
 
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest,User.digest(reset_token))
+        update_attribute(:reset_sent_at,Time.zone.now)
+    end
+
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+    
+
     private
         def downcase_email
             self.email.downcase!
