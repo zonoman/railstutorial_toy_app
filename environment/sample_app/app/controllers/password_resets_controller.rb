c23577eb3e8079a023acea313808583eac3f13c0
zonoman <morizono.2st@gmail.com>
2020-02-09 09:53:58 +0900

    12.3

--- environment/sample_app/app/controllers/password_resets_controller.rb
+++ environment/sample_app/app/controllers/password_resets_controller.rb
@@ -1,4 +1,9 @@
 class PasswordResetsController < ApplicationController
+before_action :get_user, only:[:edit, :update]
+before_action :valid_user, only:[:edit, :update]
+#パスワード再設定の有効期限がきれていないか
+before_action :check_expiration, only: [:edit,:update]
+
   def new
   end
 
@@ -18,4 +23,50 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  def update
+    if params[:user][:password].empty?#新しいパスワードが空文字列になっていないか
+      @user.errors.add(:password,:blank)
+      render 'edit'
+    elsif @user.update_attributes(user_params)#新しいパスワードがあっっていたら更新する
+      log_in @user
+      @user.update_attribute(:reset_digest,nil)
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit' #無効なパスワードっであれば失敗させる
+    end
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password,:password_confirmation)
+    end
+
+    #beforeフィルタ
+
+
+    def get_user
+      @user = User.find_by(email:params[:email])
+    end
+
+    def valid_user
+      unless (@user && @user.activated? && 
+                @user.authenticated?(:reset,params[:id]))
+        redirect_to root_url
+      end
+    end
+
+    #トークンが期限切れかどうか確認する
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
+
+
+
 end
